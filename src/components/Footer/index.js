a4169e50b7f20568a08bb59bf1e32b790d22742a
ezopas <37296410+ezopas@users.noreply.github.com>
2019-02-25 13:07:50 +0200

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -13,9 +13,9 @@ const Footer = ({}) =>{
                             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
                                 there live the blind texts.</p>
                             <ul className="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                                <li className="ftco-animate"><a href="#"><span className="icon-twitter"></span></a></li>
-                                <li className="ftco-animate"><a href="#"><span className="icon-facebook"></span></a></li>
-                                <li className="ftco-animate"><a href="#"><span className="icon-instagram"></span></a></li>
+                                <li className="ftco-animate"><a href=""><span className="icon-twitter"></span></a></li>
+                                <li className="ftco-animate"><a href=""><span className="icon-facebook"></span></a></li>
+                                <li className="ftco-animate"><a href=""><span className="icon-instagram"></span></a></li>
                             </ul>
                         </div>
                     </div>
@@ -25,24 +25,24 @@ const Footer = ({}) =>{
                             <div className="block-21 mb-4 d-flex">
                                 <a className="blog-img mr-4 k1"></a>
                                 <div className="text">
-                                    <h3 className="heading"><a href="#">Even the all-powerful Pointing has no control
+                                    <h3 className="heading"><a href="">Even the all-powerful Pointing has no control
                                         about</a></h3>
                                     <div className="meta">
-                                        <div><a href="#"><span className="icon-calendar"></span> Sept 15, 2018</a></div>
-                                        <div><a href="#"><span className="icon-person"></span> Admin</a></div>
-                                        <div><a href="#"><span className="icon-chat"></span> 19</a></div>
+                                        <div><a href=""><span className="icon-calendar"></span> Sept 15, 2018</a></div>
+                                        <div><a href=""><span className="icon-person"></span> Admin</a></div>
+                                        <div><a href=""><span className="icon-chat"></span> 19</a></div>
                                     </div>
                                 </div>
                             </div>
                             <div className="block-21 mb-4 d-flex">
                                 <a className="blog-img mr-4 k2"></a>
                                 <div className="text">
-                                    <h3 className="heading"><a href="#">Even the all-powerful Pointing has no control
+                                    <h3 className="heading"><a href="">Even the all-powerful Pointing has no control
                                         about</a></h3>
                                     <div className="meta">
-                                        <div><a href="#"><span className="icon-calendar"></span> Sept 15, 2018</a></div>
-                                        <div><a href="#"><span className="icon-person"></span> Admin</a></div>
-                                        <div><a href="#"><span className="icon-chat"></span> 19</a></div>
+                                        <div><a href=""><span className="icon-calendar"></span> Sept 15, 2018</a></div>
+                                        <div><a href=""><span className="icon-person"></span> Admin</a></div>
+                                        <div><a href=""><span className="icon-chat"></span> 19</a></div>
                                     </div>
                                 </div>
                             </div>
@@ -52,11 +52,11 @@ const Footer = ({}) =>{
                         <div className="ftco-footer-widget mb-4 ml-md-4">
                             <h2 className="ftco-heading-2">Services</h2>
                             <ul className="list-unstyled">
-                                <li><a href="#" className="py-2 d-block">Haircut</a></li>
-                                <li><a href="#" className="py-2 d-block">Hairstyle</a></li>
-                                <li><a href="#" className="py-2 d-block">Trimming</a></li>
-                                <li><a href="#" className="py-2 d-block">Shaving</a></li>
-                                <li><a href="#" className="py-2 d-block">Skin Care</a></li>
+                                <li><a href="" className="py-2 d-block">Haircut</a></li>
+                                <li><a href="" className="py-2 d-block">Hairstyle</a></li>
+                                <li><a href="" className="py-2 d-block">Trimming</a></li>
+                                <li><a href="" className="py-2 d-block">Shaving</a></li>
+                                <li><a href="" className="py-2 d-block">Skin Care</a></li>
                             </ul>
                         </div>
                     </div>
@@ -67,9 +67,9 @@ const Footer = ({}) =>{
                                 <ul>
                                     <li><span className="icon icon-map-marker"></span><span className="text">203 Fake St. Mountain View, San Francisco, California, USA</span>
                                     </li>
-                                    <li><a href="#"><span className="icon icon-phone"></span><span className="text">+2 392 3929 210</span></a>
+                                    <li><a href=""><span className="icon icon-phone"></span><span className="text">+2 392 3929 210</span></a>
                                     </li>
-                                    <li><a href="#"><span className="icon icon-envelope"></span><span
+                                    <li><a href=""><span className="icon icon-envelope"></span><span
                                         className="text">info@yourdomain.com</span></a></li>
                                 </ul>
                             </div>
